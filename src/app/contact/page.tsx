4a3fc208af8f25bb3ec418164ba0136b2bfbe304
famousshea <sheamus@famoussheamus.com>
2026-05-27 13:26:31 -0500

    feat: add YouTube social button to home page and contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -44,11 +44,19 @@ const RedditIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+// Custom SVG for YouTube logo
+const YoutubeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+  </svg>
+);
+
 const SOCIAL_LINKS = [
   { name: "LinkedIn", icon: Linkedin, href: "https://www.linkedin.com/in/sheamus-byrne/", color: "hover:bg-[#0077b5] hover:text-white hover:border-[#0077b5]" },
   { name: "X / Twitter", icon: XIcon, href: "https://x.com/famousshea", color: "hover:bg-black hover:text-white hover:border-black dark:hover:bg-zinc-800" },
   { name: "GitHub", icon: Github, href: "https://github.com/famousshea", color: "hover:bg-zinc-900 hover:text-white hover:border-zinc-900 dark:hover:bg-zinc-800" },
   { name: "Reddit", icon: RedditIcon, href: "https://www.reddit.com/user/FamousSheamusAI/", color: "hover:bg-[#FF4500] hover:text-white hover:border-[#FF4500]" },
+  { name: "YouTube", icon: YoutubeIcon, href: "https://www.youtube.com/@famoussheamusai", color: "hover:bg-[#FF0000] hover:text-white hover:border-[#FF0000]" },
 ];
 
 export default function ContactPage() {
--- src/components/HeroGreeting.tsx
+++ src/components/HeroGreeting.tsx
@@ -43,12 +43,20 @@ const RedditIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+// Custom SVG for YouTube logo
+const YoutubeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+  </svg>
+);
+
 export function HeroGreeting() {
   const socialLinks = [
     { icon: Linkedin, href: "https://www.linkedin.com/in/sheamus-byrne/", color: "hover:text-blue-600" },
     { icon: XIcon, href: "https://x.com/famousshea", color: "hover:text-zinc-900 dark:hover:text-white" },
     { icon: Github, href: "https://github.com/famousshea", color: "hover:text-zinc-900 dark:hover:text-white" },
     { icon: RedditIcon, href: "https://www.reddit.com/user/FamousSheamusAI/", color: "hover:text-[#FF4500]" },
+    { icon: YoutubeIcon, href: "https://www.youtube.com/@famoussheamusai", color: "hover:text-[#FF0000]" },
   ];
 
   return (
